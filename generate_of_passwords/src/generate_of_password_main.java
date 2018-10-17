fdfbc1d625d367be373d5ca308b8a4973168e6fd
yahya <yahyaislamov15@mail.ru>
2018-10-18 00:25:35 +0300

    начал работу над созданием пароля

---
+++ generate_of_passwords/src/generate_of_password_main.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+public class generate_of_password_main {
+    public static void main(String[] args) {
+        if (IsCorrectCountOfArgs(args)) {
+
+        }
+    }
+
+    private static boolean IsCorrectCountOfArgs(String[] args) {
+        return (args.length  == 3);
+    }
+
+    private static String getPassword
+}
--- сaesar_cipher/src/Caesar_cipher_main.java
+++ сaesar_cipher/src/Caesar_cipher_main.java
@@ -6,14 +6,14 @@ public class Caesar_cipher_main {
             String operationMode = args[0];
             if (operationMode.contains("-e")) {
                 try {
-                    String encodingWord = EncodeAWord(args[2], Integer.parseInt(args[1]));
+                    String encodingWord = GetEncodeAWord(args[2], Integer.parseInt(args[1]));
                     System.out.println(encodingWord);
                 } catch (NumberFormatException e) {
                     System.out.println("Вторым аргументом вы передали не цифру");
                 }
             } else if (operationMode.contains("-d")) {
                 try {
-                    String decodingWord = DecodeAWord(args[2], Integer.parseInt(args[1]));
+                    String decodingWord = GetDecodeAWord(args[2], Integer.parseInt(args[1]));
                     System.out.println(decodingWord);
                 } catch (NumberFormatException e) {
                     System.out.println("Вторым аргументом вы передали не цифру");
@@ -30,7 +30,7 @@ public class Caesar_cipher_main {
         return (args.length  == 3);
     }
 
-    private static String EncodeAWord(String word, int key) {
+    private static String GetEncodeAWord(String word, int key) {
         String newString = new String();
         for(int i = 0; i < word.length(); i++)
             newString += ((char)((int)word.charAt(i) + key));
@@ -38,7 +38,7 @@ public class Caesar_cipher_main {
         return newString;
     }
 
-    private static String DecodeAWord(String word, int key) {
+    private static String GetDecodeAWord(String word, int key) {
         String newString  = new String();
         for(int i = 0; i < word.length(); i++)
             newString += ((char)((int)word.charAt(i) - key));
