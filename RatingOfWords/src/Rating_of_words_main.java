3dde8ae8fd3088e3983897ef320e91bbcb20be10
yahya <yahyaislamov15@mail.ru>
2018-10-28 20:17:21 +0300

    небольшие изменения

--- RatingOfWords/src/Rating_of_words_main.java
+++ RatingOfWords/src/Rating_of_words_main.java
@@ -7,53 +7,66 @@ import java.security.KeyStore;
 import java.util.*;
 
 public class Rating_of_words_main {
-    public static void main(String[] args) {
-        if (IsCorrectCountOfArgs(args)) {
-            File file = new File(args[0]);
-            try {
-                BufferedReader fin = new BufferedReader(new FileReader(file));
-                int count = Integer.parseInt(args[1]);
-                int checkCount = 0;
-                HashMap<String, Integer> mapWithLines = new HashMap<String, Integer>();
-                List<String> arrForCheck = new ArrayList<String>();
-                String line = new String();
+  public static void main(String[] args) {
+    if (IsCorrectCountOfArgs(args)) {
+      File file = new File(args[0]);
+      try {
+        BufferedReader fin = new BufferedReader(new FileReader(file));
+        int count = Integer.parseInt(args[1]);
+        int checkCount = 0;
+        HashMap<String, Integer> mapWithLines = new HashMap<String, Integer>();
+        List<String> arrForCheck = new ArrayList<String>();
+        int numberOfchar = -1;
+        String line = new String();
+        char letter;
+        while (true) {
+          numberOfchar = fin.read();
 
-                while (true) {
-                    line = fin.readLine();
-                    if (line.length() == 0)
-                        break;
+          if (numberOfchar == -1)
+            break;
 
-                    Collections.addAll(arrForCheck, line.split(" "));
-                    for (int i = 0; i < arrForCheck.size(); i++) {
-                        if (mapWithLines.containsKey(arrForCheck.get(i))) {
-                            mapWithLines.put(arrForCheck.get(i), mapWithLines.get(arrForCheck.get(i)) + 1);
-                        } else {
-                            checkCount++;
-                            if (checkCount <= count)
-                                mapWithLines.put(arrForCheck.get(i), 1);
-                        }
+          while (numberOfchar != -1 && numberOfchar != (int) ' ') {
+            letter = (char) numberOfchar;
+            line += letter;
+            numberOfchar = fin.read();
+          }
 
-                        if (checkCount > count)
-                            break;
-                    }
+          if (mapWithLines.containsKey(line))
+            mapWithLines.put(line, mapWithLines.get(line) + 1);
+          else {
+            checkCount++;
+            if (checkCount <= count)
+              mapWithLines.put(line, 1);
+          }
 
-                    if (checkCount > count)
-                        break;
-                    arrForCheck.clear();
 
-                }
-
-                fin.close();
-                for (Map.Entry<String, Integer> myPair : mapWithLines.entrySet())
-                    System.out.println(myPair.getKey() + ' ' + myPair.getValue());
-
-            } catch (IOException error) {
-                System.out.println(error.toString());
-            }
+          if (checkCount > count)
+            break;
         }
-    }
 
-    private static boolean IsCorrectCountOfArgs(String[] args) {
-        return (args.length == 2);
+        fin.close();
+        for (Map.Entry<String, Integer> myPair : mapWithLines.entrySet())
+          System.out.println(myPair.getKey() + ' ' + myPair.getValue());
+
+      } catch (IOException error) {
+        System.out.println(error.toString());
+      }
     }
+  }
+
+  private static boolean IsCorrectCountOfArgs(String[] args) {
+    return (args.length == 2);
+  }
+
+  public static void SortHashMapKey(Map unsortedMap) {
+    List<Integer> list = new ArrayList(unsortedMap.values());
+    Collections.sort(list, new Comparator<Integer>() {
+
+      @Override
+      public int compare(Integer p1, Integer p2) {
+        return p1 - p2;
+      }
+    });
+  }
+
 }
